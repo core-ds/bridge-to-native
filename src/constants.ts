f36761a24c4ecff8b64e4d5f4c1786b4e50631dd
Evgen Shmat <evgeniy.shmat@gmail.com>
2024-02-14 13:29:17 +0300

    feat(android): encode android_app_id

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { NativeFeaturesFromVersion } from './types';
 
 export const START_VERSION_ANDROID_AM_ALLOW_OPEN_NEW_WEBVIEW = '10.35.0';
-export const ANDROID_APP_ID = 'alfabank';
+export const ANDROID_APP_ID = 'YWxmYWJhbms=';
 export const CLOSE_WEBVIEW_SEARCH_KEY = 'closeWebView';
 export const CLOSE_WEBVIEW_SEARCH_VALUE = 'true';
 export const PREVIOUS_B2N_STATE_STORAGE_KEY = 'previousBridgeToNativeState';
