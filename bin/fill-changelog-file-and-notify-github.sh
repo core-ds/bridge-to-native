415d93ed2da2821d407df349ff9e46030262f1a3
Демидов Артем Николаевич <ADemidov3@alfabank.ru>
2025-11-26 12:25:06 +0300

    feat(*): update release workflow

--- bin/fill-changelog-file-and-notify-github.sh
+++ bin/fill-changelog-file-and-notify-github.sh
@@ -7,53 +7,24 @@ separator="
 
 # Генерирует title для записи в changelog в формате: # 2.2.8 (24ewe456) (01.01.2024)
 function generateTitle {
-  if [[ $SEMANTIC_VERSION == "patch" ]]
-    then caption="###"
-  elif [[ $SEMANTIC_VERSION == "minor" ]]
-    then caption="##"
-  elif [[ $SEMANTIC_VERSION == "major" ]]
-    then caption="#"
-  fi
-
   gmt_time=$(TZ="GMT+3" date +%d-%m-%Y);
 
-  echo "$caption $VERSION ([$SHA_SHORT]($LAST_COMMIT_URL)) ($gmt_time)"
+  echo "## $VERSION ([$SHA_SHORT]($LAST_COMMIT_URL)) ($gmt_time)"
 };
 
-# Возвращает строку в markdown разметке о Features, Bug Fixes, BREAKING CHANGES (если что-то из этого отсутствует, то в не попадет на выход)
-function generateChangelogInfo {
-  local ChangelogInfo="";
+# Берем описание changelog из github релиза
+RELEASE_BODY=$(jq -r '.release.body' "$GITHUB_EVENT_PATH")
 
-  if [ ${#FEATURES} -gt 0 ]
-    then ChangelogInfo+="$new_line### Features${FEATURES//\//$separator}"
-  fi
-
-  if [ ${#BUGFIXES} -gt 0 ]
-    then ChangelogInfo+="$new_line### Bug Fixes${BUGFIXES//\//$separator}"
-  fi
-
-  if [ ${#BREAKING_CHANGES} -gt 0 ]
-    then ChangelogInfo+="$new_line### BREAKING CHANGES${BREAKING_CHANGES//\//$separator}"
-  fi
-
-  echo "$ChangelogInfo$new_line"
-};
-
-# Передает в summary информацию о Features, Bug Fixes, BREAKING CHANGES, или об ее отсутствии
-function checkAvailabilityInformationForChangelog {
-  if [[ ${#FEATURES} -eq 0 && ${#BUGFIXES} -eq 0 && ${#BREAKING_CHANGES} -eq 0  ]]
-    then echo "Не указана информация об изменениях для заполнения $changelog_file файла." >> "$GITHUB_STEP_SUMMARY"
-  else echo "$Changelog" >> "$GITHUB_STEP_SUMMARY"
-  fi
-}
-
-checkAvailabilityInformationForChangelog
+if [[ -z "$RELEASE_BODY" ]]; then
+  echo "⚠️ В release body нет описания изменений для заполнения $changelog_file." >> "$GITHUB_STEP_SUMMARY"
+else
+  echo "$RELEASE_BODY" >> "$GITHUB_STEP_SUMMARY"
+fi
 
 Title=$(generateTitle)
-Changelog=$(generateChangelogInfo)
 
 fullChangelogInfo="$Title
-$Changelog
+$RELEASE_BODY
 "
 
 # Отправляет информацию о релизе и содержимое .public в github release
@@ -99,5 +70,3 @@ postReleaseMessage
 echo "$fullChangelogInfo" | cat - "$changelog_file" > temp && mv temp "$changelog_file"
 
 echo "# Выпущена новая версия библиотеки: $VERSION" >> "$GITHUB_STEP_SUMMARY"
-
-checkAvailabilityInformationForChangelog
