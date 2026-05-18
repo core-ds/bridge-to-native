f7c96c2db6444f0de09ba735455f28ad6a870772
Демидов Артем Николаевич <ADemidov3@alfabank.ru>
2026-05-18 10:09:02 +0300

    feat(*): change test namings

--- __tests__/client/services-and-utils/native-execute-service.test.ts
+++ __tests__/client/services-and-utils/native-execute-service.test.ts
@@ -1,41 +1,39 @@
 import { NativeExecuteService } from '../../../src/client/services-and-utils/native-execute-service';
 
-describe('ExternalLinksService', () => {
+describe('NativeExecuteService', () => {
     beforeEach(() => {
         jest.clearAllMocks();
     });
 
-    describe('method `handleNativeDeeplink`', () => {
-        it('should not execute fn when noop is enabled', () => {
-            const fn = jest.fn();
+    it('should not execute fn when noop is enabled', () => {
+        const fn = jest.fn();
 
-            const service = new NativeExecuteService(true, 'ios');
+        const service = new NativeExecuteService(true, 'ios');
 
-            service.execute('test', fn);
+        service.execute('test', fn);
 
-            expect(fn).not.toHaveBeenCalled();
-        });
+        expect(fn).not.toHaveBeenCalled();
+    });
 
-        it('should execute fn when noop is disabled', () => {
-            const fn = jest.fn();
+    it('should execute fn when noop is disabled', () => {
+        const fn = jest.fn();
 
-            const service = new NativeExecuteService(false, 'ios');
+        const service = new NativeExecuteService(false, 'ios');
 
-            service.execute('test', fn);
+        service.execute('test', fn);
 
-            expect(fn).toHaveBeenCalled();
-        });
+        expect(fn).toHaveBeenCalled();
+    });
 
-        it('should include environment in noop log', () => {
-            console.info = jest.fn();
+    it('should include environment in noop log', () => {
+        console.info = jest.fn();
 
-            const service = new NativeExecuteService(true, 'android');
+        const service = new NativeExecuteService(true, 'android');
 
-            service.execute('test action', jest.fn(), { foo: 'bar' });
+        service.execute('test action', jest.fn(), { foo: 'bar' });
 
-            expect(console.info).toHaveBeenCalledWith('[B2N noop][android] test action', {
-                foo: 'bar',
-            });
+        expect(console.info).toHaveBeenCalledWith('[B2N noop][android] test action', {
+            foo: 'bar',
         });
     });
 });
